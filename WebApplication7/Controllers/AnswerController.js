4ec218fd200e95d0a54d8686ef99d7e0391e20a2
Anton Bialenik <tony.baton@yandex.by>
2018-10-02 10:36:04 +0300

    added form for "new answer"

---
+++ WebApplication7/Controllers/AnswerController.js
@@ -0,0 +1,7 @@
+﻿questApp.controller('AnswerController', function AnswerController($scope) {
+    $scope.save = function(answer, answerForm) {
+        if (answerForm.$valid) {
+            alert(answer.author + ", Ваш ответ сохранен");
+        }
+    };
+})
