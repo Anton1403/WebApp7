2bd62f7258f814747fd1493638571c131f37053f
Anton Bialenik <tony.baton@yandex.by>
2018-10-03 10:39:01 +0300

    DOM structure, add 1 js file 'DOM.js' with function

---
+++ WebApplication7/Js/DOM.js
@@ -0,0 +1,14 @@
+﻿var quest = angular.element(document.querySelector(".quest"));
+var questHeader = quest.find('h3');
+questHeader.remove();
+var newDiv = angular.element("<div>");
+newDiv.css("width", "80px");
+newDiv.css("height", "50px");
+newDiv.css("background-color", "blue");
+newDiv.text("Hello");
+newDiv.on("click",
+    function(e) {
+        var w = parseInt(newDiv.css("width")) + 10;
+        newDiv.css("width", w + "px");
+    });
+quest.append(newDiv);
