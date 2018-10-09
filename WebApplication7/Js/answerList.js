df97bc41fcce3b54e9ec3f9be6f18a1ad352c465
Anton Bialenik <tony.baton@yandex.by>
2018-10-09 11:01:16 +0300

    no message

--- WebApplication7/Js/answerList.js
+++ WebApplication7/Js/answerList.js
@@ -4,12 +4,21 @@
         var data = scope[attrs["answerList"]];
 
         if (angular.isArray(data.answers)) {
+            var divElem = angular.element("<div>").addClass("answers");
+            element.append(divElem);
             var ulElem = angular.element("<ul>");
             element.append(ulElem);
             for (var i = 0; i < data.answers.length; i++) {
                 var liElem = angular.element('<li>');
                 liElem.append(angular.element('<p>').text(data.answers[i].text));
                 ulElem.append(liElem);
+
+                var answerElem = angular.element('<div>').addClass("answer");
+                answerElem.append(angular.element('<b>').text(data.answers[i].author));
+                liElem.append(answerElem);
+                var iElem = angular.element('<i>').text(data.answers[i].date)
+                var dateElem = angular.element('<p>').append(iElem);
+                answerElem.append(dateElem);
             }
         }
     }
